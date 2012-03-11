8319db04d9209fc04731d30144fc442d5b15bd19
Jesse Stuart <jessestuart@gmail.com>
2012-03-11 18:48:30 -0400

    Init Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchApartments::Application.config.secret_token = 'e14a00254dd7db5cbb50f68096df8dc203ff376745708945f96240e05c61c0babb18395698c2bd14c14bc783bf9c5e73ce9aeb315e0506156d1963441a202cd9'
